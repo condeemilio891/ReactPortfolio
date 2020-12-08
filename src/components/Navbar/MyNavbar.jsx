472d96be42c9ad99c6675f85fa57f3f3c3838677
Emilio Conde <condeemilio891@gmail.com>
2020-12-08 14:34:20 -0600

    bootstrap

---
+++ src/components/Navbar/MyNavbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./style.css";
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+export const MyNavbar = () => {
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+          </Nav>
+         
+        </Navbar.Collapse>
+      </Navbar> </div>
+  )
+}
+
+export default MyNavbar;
--- src/pages/Discover.js
+++ src/pages/Discover.js
@@ -2,21 +2,29 @@ import React, { Component } from "react";
 import Card from "../components/Card/cardui";
 import Tab from "../components/Tabs/tab";
 import TabNav from "../components/Tabs/tabNav";
-import Yoda from "../assets/yoda.jpeg"
-
+import AnimePic from "../assets/animepic.png"
+import Hero from "../components/Hero"
+import Container from "../components/Container"
 function Projects ()
     {
         return (
           <div>
             <h1>Projects</h1>
            <TabNav> 
+             
              <div label="Project 1"> 
-             See ya later, <em>Project 1</em>!
-             <Card imgsrc={Yoda}>
+             WatchIt <em>Search for your favorite Anime</em>!
+             <Container>
+             <Card imgsrc={AnimePic}>
                 </Card>
+                </Container>
              </div> 
              <div label="Project 2"> 
+             <Container>
+               <Hero backgroundImage={AnimePic}></Hero>
+             </Container>
                After 'while, <em>Crocodile</em>! 
+             
              </div> 
              <div label="Project 3"> 
                Nothing to see here, this tab is <em>extinct</em>! 
