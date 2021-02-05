64987f83b5da263282e7db8522cab0cc05505727
Emilio Conde <condeemilio891@gmail.com>
2021-02-04 22:47:50 -0600

    improved footer added items and links to footer added background added brand statement

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import Projects from "./pages/Portfolio/Portfolio";
 import About from "./pages/About/About";
-import Search from "./pages/Search";
+import Footer from "./components/Footer/Footer"
 import MyNavbar from "./components/Navbar/MyNavbar";
-import Footer from "./components/Footer/Footer.js";
+
 import Wrapper from "./components/Wrapper";
 
 function App() {
@@ -19,9 +19,11 @@ function App() {
           <Route exact path="/portfolio" component={Projects}/> 
           
         </Wrapper>
-        {/* <Footer /> */}
+        
       </div>
+      
     </Router>
+    
   );
 }
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './footer.css';
-// import { Button } from '../Navbar/Button';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import 'moment-timezone';
@@ -16,38 +15,86 @@ function Footer() {
 
 
   return (
-
-   <div className='footer-container'>
+    
+    <div className='footer-container'>
+          <div className='footer-links'>
+        <div className='footer-link-wrapper'>
       
-      {/* <!-- Footer social --> */}
-      <section className="ft-social">
-        <div className="ft-social-wrap">
-        <ul class="ft-social-list">
-          <li><a href="#"><i class="fab fa-facebook"></i></a></li>
-          <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-          <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-          <li><a href="#"><i class="fab fa-github"></i></a></li>
-          <li><a href="#"><i class="fab fa-linkedin"></i></a></li>
-          <li><a href="#"><i class="fab fa-youtube"></i></a></li>
-        </ul>
+        </div>
+      </div>
+    
+    
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <div className='footer-logo'>
+            <Link to='/' className='social-logo'>
+              <i className='fas fa-microscope' />
+            </Link>
+      </div>
+        
+          <div className='social-icons'>
+            <a className ="social-icon-link" href="https://docs.google.com/document/d/1Rbz5gWwFHqjOh78-TLTi_wcWiNxMNhJJLNRJkclJYGI/edit?usp=sharing" variant="link">Resume</a>
+            
+            <a className ="social-icon-link"  href='https://github.com/condeemilio891' varient="link">Github</a>
+            
+            <a className ="social-icon-link"  href='https://www.linkedin.com/in/emilio-j-conde-ab2101195/' varient="link">LinkedIn</a>
+            
+            <a className ="social-icon-link"  href='#portfolio'>Portfolio</a>
+          
+          </div>
+          
+         
+          <p className='website-rights'>
+          <Moment component={dateToFormat} /> 
+          </p>
+
+          <div className='social-icons'>
+
+            <a 
+                className ="social-icon-link twitter"
+                href="https://github.com/condeemilio891/EmployeeManagementSystem"
+                target= '_blank'
+                aria-label
+              a> 
+              <i className='fab fa-github' />
+              Employee Management System
+              </a> 
+              <a 
+                className ="social-icon-link twitter"
+                href="https://github.com/condeemilio891/good_READme_generator"
+                target= '_blank'
+                aria-label
+              a> 
+              <i className='fab fa-github' />
+              GoodReadme Generator
+              </a> 
+            
+              <a 
+                className ="social-icon-link twitter"
+                href="https://condeemilio891.github.io/quiz/"
+                target= '_blank'
+                aria-label
+              a> 
+              <i className='fab fa-github' />
+              Try This NeuroBiology Quiz
+              </a> 
+              <a 
+                className ="social-icon-link twitter"
+                href="https://game-dex-ut.herokuapp.com/"
+                target= '_blank'
+                aria-label
+              a> 
+              <i className='fab fa-github' />
+              GameDex
+          </a>
+          </div>
+          <Link to='/' className='social-logo'>
+              <i className='fas fa-microscope' />
+            </Link>
         </div>
       </section>
-     
-      {/* <!-- Footer legal --> */}
-      <section class="ft-legal">
-        <ul class="ft-legal-list">
-          <li><a href="#">Terms &amp; Conditions</a></li>
-          <li><a href="#">Privacy Policy</a></li>
-          <li>&copy; 2019 Copyright Nowrap Inc.</li>
-        </ul>
-      </section>
-
-      
-
-      </div>
-  
-  
-    )   
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
