db0c0aa0184afb8514449e22a51db735d911ea30
Emilio Conde <condeemilio891@gmail.com>
2021-04-06 09:48:04 -0500

    looks better

--- src/components/Title/title.js
+++ src/components/Title/title.js
@@ -6,9 +6,11 @@ import "./title.css"
 const Textbox=(props)=>{
     return(
         <>
+        <div id="cyanBG">
         <h2 id="name">{props.name}</h2>
 
         <h6 id='subTitle'>{props.subTitle}</h6>
+        </div>
         </>
     )
 }
--- src/pages/onePage/onePage.jsx
+++ src/pages/onePage/onePage.jsx
@@ -2,13 +2,22 @@ import React from "react"
 import Projects from '../Portfolio/Portfolio'
 import Textbox from "../../components/Title/title"
 import Radio from "../../components/Radio/Radio"
+import Container from "react-bootstrap/Container"
+import Video from "../../assets/videos/spaceVid.mp4";
 import "./onePage.css"
 const onePage =()=>{
 
  return(
      
-     <>
      
+      
+
+<div id="about">
+      <div className="about">
+        <Container id="bg1" className="mt-0">
+          <video className="videoTag" autoPlay loop muted>
+            <source src={Video} type="video/mp4" />
+          </video>
      <Textbox
        name= "Emilio Conde"
        subTitle="Front-End Developer"/>
@@ -26,7 +35,13 @@ const onePage =()=>{
               </div>
        <Radio/>
     <Projects/>
-    </>
+    </Container>
+       
+       </div>
+     </div>
+   
+    
+       
     
  )
 
