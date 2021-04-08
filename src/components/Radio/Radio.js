7b2b250ff61a70e966c64e90b3577c3a0352937a
Emilio Conde <condeemilio891@gmail.com>
2021-04-08 00:13:00 -0500

    radioStuff

--- src/components/Radio/Radio.js
+++ src/components/Radio/Radio.js
@@ -6,26 +6,44 @@ import "./Radio.css"
 
 function Radio() {
   const [songs] = useState([
-    { title:"Luminescence (Stellaris)",
+    { title:"Luminescence (Stellaris-2016)",
       artist: "Andres Waldorf",
       img_src:"/images/Stellaris.jpg",
       src: process.env.PUBLIC_URL+"/music/luminescence.mp3"
       
     },
     {
-      title:" Space Jam theme",
-      artist:"Quad City DJ's",
-      img_src:"/images/spacejamcover.jpg",
-      src: process.env.PUBLIC_URL +"/music/SpaceJamThemeSong.mp3"
+      title:"Alison (1993)",
+      artist:"SlowDive ",
+      img_src:"/images/Souvlaki.jpg",
+      src:process.env.PUBLIC_URL+"/music/Alison.mp3"
+    },
+    {title:"Time (1973)",
+      artist: "Pink Floyd ",
+      img_src:"/images/DarkSide.jpg",
+      src: process.env.PUBLIC_URL+"/music/Time.mp3"
+      },
+    {
+      title:"Only Tommorow (2013)",
+      artist: "My Bloody Valentine ",
+      img_src:"/images/MBV.jpg",
+      src: process.env.PUBLIC_URL+"/music/onlyTomorrow.mp3"
     },
     {
-      title: "Terran 3 (Starcraft)",
-      artist: "Blizzard Entertainment",
+      title: "Terran 3 (Starcraft-1998)",
+      artist: "Blizzard Entertainment ",
       img_src: "/images/starcraft-original-box.webp",
       src: process.env.PUBLIC_URL+ "/music/Terran3Starcraft.mp3"
     },
     {
-      title: "Faster Than Light",
+      title:" Space Jam (1996)",
+      artist:"Quad City DJ's",
+      img_src:"/images/spacejamcover.jpg",
+      src: process.env.PUBLIC_URL +"/music/SpaceJamThemeSong.mp3"
+    },
+   
+    {
+      title: "Faster Than Light (2016)",
       artist: "Andreas Waldetoft (feat. Mia Stegmar)",
       img_src:"/images/Stellaris.jpg",
       src: process.env.PUBLIC_URL +"/music/FTL.mp3"
